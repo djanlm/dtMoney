1c42e38642fa84c080ee4b843e85051bd8f7a7fc
Djanilson Martins <djanlm@hotmail.com>
2021-06-26 19:00:25 -0300

    module completed

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,12 @@ import Modal from 'react-modal';
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
 import { NewTransactionModal } from './components/NewTransactionModal';
-
+import { TransactionsProvider } from './hooks/useTransactions';
 import { GlobalStyle } from "./styles/global";
 
 Modal.setAppElement('#root');
 
-/*const Title = styled.h1`
-  font-size: 64px;
-  color: #8257e6;
-`*/
+
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
 
@@ -24,7 +21,7 @@ export function App() {
     setIsNewTransactionModalOpen(false)
   }
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <NewTransactionModal
@@ -32,7 +29,7 @@ export function App() {
         onRequestClose={handleCloseNewTransactionModal}
       />
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
 
